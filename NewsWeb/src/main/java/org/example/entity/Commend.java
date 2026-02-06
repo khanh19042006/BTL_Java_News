8573d082140843ffe6c41f155ec354ad8d94203a
khanh19042006 <dinhkhanh19042006@gmail.com>
2026-02-06 17:37:40 +0700

    feature: create entity commend

--- NewsWeb/src/main/java/org/example/entity/Commend.java
+++ NewsWeb/src/main/java/org/example/entity/Commend.java
@@ -4,5 +4,64 @@ public class Commend {
     private String id;
     private String content;
     private String authorId;
+    private String newsId;
+    private String timeUp;
+    private int level;
 
+    public Commend(String id, String content, String authorId, String newsId, String timeUp, int level) {
+        this.id = id;
+        this.content = content;
+        this.authorId = authorId;
+        this.newsId = newsId;
+        this.timeUp = timeUp;
+        this.level = level;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(String newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getTimeUp() {
+        return timeUp;
+    }
+
+    public void setTimeUp(String timeUp) {
+        this.timeUp = timeUp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
