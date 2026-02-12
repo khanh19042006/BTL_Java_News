f804efd098a2fbb3bf64c856caa79c8ee2d26e7d
khanh19042006 <dinhkhanh19042006@gmail.com>
2026-02-13 00:50:15 +0700

    clean: clean tree

--- NewsWeb/src/main/java/org/example/controller/ProfileController.java
+++ NewsWeb/src/main/java/org/example/controller/ProfileController.java
@@ -33,7 +33,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.example.controller.NewsDetailController;
 
 public class ProfileController {
 
